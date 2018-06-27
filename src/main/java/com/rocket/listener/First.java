5299b2a8b495fe4db67469811f6b26d468f094a0
YuQuan-Liu <liuyuquan08ouc@gmail.com>
2018-06-27 15:58:10 +0800

    first blood
    maven pom OK

---
+++ src/main/java/com/rocket/listener/First.java
@@ -0,0 +1,11 @@
+package com.rocket.listener;
+
+/**
+ * Created by Rocket on 2018/6/26.
+ */
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/scala/com/rocket/listener/TestScala.scala
@@ -0,0 +1,10 @@
+package com.rocket.listener
+
+/**
+  * Created by Rocket on 2018/6/27.
+  */
+object TestScala {
+    def main(args: Array[String]) {
+        print("Hello")
+    }
+}
